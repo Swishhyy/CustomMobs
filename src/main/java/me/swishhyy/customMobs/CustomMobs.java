f29b10e81bd434d7c7012d2e6a1789ec25c3e2e8
Swishhyy <72903966+Swishhyy@users.noreply.github.com>
2025-09-11 14:16:46 -0500

    Project Init

---
+++ src/main/java/me/swishhyy/customMobs/CustomMobs.java
@@ -0,0 +1,17 @@
+package me.swishhyy.customMobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomMobs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
